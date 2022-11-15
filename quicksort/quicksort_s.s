a4f80ac88650343a599d7436849e4c19e06e7606
Leonard Pouliot <leonard.pouliot@polymtl.com>
2022-11-15 08:33:15 -0500

    Debut de quicksort

--- quicksort/quicksort_s.s
+++ quicksort/quicksort_s.s
@@ -4,7 +4,27 @@ quicksort_s:
 pushl %ebp
 movl %esp, %ebp
 pushl %ebx
+
 # DEBUT COMPLETION
+movl 12(%esp), %eax #topTab
+movl 16(%esp), %ebx #left 0
+movl 20(%esp), %ecx #right 8
+
+print :
+pushl %ecx
+pushl %eax
+call printT
+popl %eax
+popl %ecx
+
+
+movl %ebx, %edx
+addl $2, %edx
+cmpl %ecx, %edx
+jnle retour
+
+
+
 
 # FIN COMPLETION
 # NE RIEN MODIFIER APRES CETTE LIGNE
